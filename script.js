182b01b55c3275e51ab3f9ba8708581632d9ed56
JordanC <jcwilliams410@gmail.com>
2020-10-14 00:14:45 -0400

    working aggressive algorithm

--- script.js
+++ script.js
@@ -23,22 +23,28 @@ function showHamburgerLinks() {
     mounted: function () {
       this.aggressiveSongs()
       this.whimsicalSongs()
+      this.spookySongs()
     },
     methods: {
       aggressiveSongs: function(){
         fetch('https://spreadsheets.google.com/feeds/list/1H5S6Vc-gCOCKLvQmfjfJmG2THtDb5Z_LQGaZJpWZQ4c/1/public/values?alt=json')
         .then(response => response.json())
         .then(data => {
-          const aggressiveFeed = data.feed.entry.map(entry => {
-            if (entry.gsx$danceability.$t > 0.7) {
+          // entry.gsx$acousticness.$t < 0.1 && entry.gsx$danceability.$t < 0.6
+          let aggressiveFeed = data.feed.entry.map(entry => {
+            if (entry.gsx$energy.$t > 0.65 && entry.gsx$valence.$t < 0.5) {
               return {
                 title: entry.gsx$songtitle.$t,
-                artist: entry.gsx$artist.$t
+                artist: entry.gsx$artist.$t,
+                energy: entry.gsx$energy.$t
               }
             } 
           })
           .filter(entry => entry !== undefined);
-          this.aggressive = aggressiveFeed;
+          // sort by energy and make new array with the first 100 songs
+          aggressiveFeed = aggressiveFeed.sort((a, b) => b.energy > a.energy ? 1: -1).splice(0, 100);
+          // assign array to 'aggressive' data property
+          this.aggressive = aggressiveFeed
         })
       },
       whimsicalSongs: function(){
@@ -57,7 +63,22 @@ function showHamburgerLinks() {
           .filter(entry => entry !== undefined);
           this.whimsical = whimsicalFeed;
         })
+      },
+      spookySongs: function(){
+        fetch('https://spreadsheets.google.com/feeds/list/1H5S6Vc-gCOCKLvQmfjfJmG2THtDb5Z_LQGaZJpWZQ4c/1/public/values?alt=json')
+        .then(response => response.json())
+        .then(data => {
+          const spookyFeed = data.feed.entry.map(entry => {
+            if (entry.gsx$danceability.$t < 0.7) {
+              return {
+                title: entry.gsx$songtitle.$t,
+                artist: entry.gsx$artist.$t
+              }
+            } 
+          })
+          .filter(entry => entry !== undefined);
+          this.spooky = spookyFeed;
+        })
       }
-      // spooky:
     }
   })
