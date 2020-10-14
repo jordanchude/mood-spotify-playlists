bead915c4809436c971386f5946a70fa2f9eb6d2
JordanC <jcwilliams410@gmail.com>
2020-10-13 23:16:27 -0400

    mobile skeleton complete

--- script.js
+++ script.js
@@ -22,34 +22,42 @@ function showHamburgerLinks() {
     },
     mounted: function () {
       this.aggressiveSongs()
+      this.whimsicalSongs()
     },
     methods: {
       aggressiveSongs: function(){
         fetch('https://spreadsheets.google.com/feeds/list/1H5S6Vc-gCOCKLvQmfjfJmG2THtDb5Z_LQGaZJpWZQ4c/1/public/values?alt=json')
         .then(response => response.json())
         .then(data => {
-          // const aggressiveFeed = data.feed.entry.map(entry => {
-          //   return {
-              // title: entry.gsx$songtitle.$t,
-              // artist: entry.gsx$artist.$t
-          //   }
-          // })
-
           const aggressiveFeed = data.feed.entry.map(entry => {
-            console.log("scanned");
             if (entry.gsx$danceability.$t > 0.7) {
               return {
                 title: entry.gsx$songtitle.$t,
                 artist: entry.gsx$artist.$t
               }
-            } else {
-              return false;
-            }
+            } 
           })
+          .filter(entry => entry !== undefined);
           this.aggressive = aggressiveFeed;
         })
+      },
+      whimsicalSongs: function(){
+        fetch('https://spreadsheets.google.com/feeds/list/1H5S6Vc-gCOCKLvQmfjfJmG2THtDb5Z_LQGaZJpWZQ4c/1/public/values?alt=json')
+        .then(response => response.json())
+        .then(data => {
+          const whimsicalFeed = data.feed.entry.map(entry => {
+            console.log("scanned");
+            if (entry.gsx$danceability.$t < 0.7) {
+              return {
+                title: entry.gsx$songtitle.$t,
+                artist: entry.gsx$artist.$t
+              }
+            } 
+          })
+          .filter(entry => entry !== undefined);
+          this.whimsical = whimsicalFeed;
+        })
       }
-      // whimsical:
       // spooky:
     }
   })
